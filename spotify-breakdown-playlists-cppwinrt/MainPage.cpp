71acbe99a2f7a733c9fa6d85576d0cb43d0ff654
Debalin Das <debalin90@gmail.com>
2019-09-29 18:06:08 -0700

    Initial commit.
     + Includes blank C++/WinRT App.
     + MainPage button leads to Spotify authentication via WebAuthenticationBroker.

---
+++ spotify-breakdown-playlists-cppwinrt/MainPage.cpp
@@ -0,0 +1,46 @@
+﻿#include "pch.h"
+#include "MainPage.h"
+#include "MainPage.g.cpp"
+
+using namespace winrt;
+using namespace Windows::UI::Xaml;
+using namespace winrt::Windows::Security::Authentication::Web;
+using namespace winrt::Windows::Foundation;
+
+namespace winrt::spotify_breakdown_playlists_cppwinrt::implementation
+{
+    MainPage::MainPage()
+    {
+        InitializeComponent();
+    }
+
+    int32_t MainPage::MyProperty()
+    {
+        throw hresult_not_implemented();
+    }
+
+    void MainPage::MyProperty(int32_t /* value */)
+    {
+        throw hresult_not_implemented();
+    }
+
+    IAsyncAction MainPage::ClickHandler(IInspectable const&, RoutedEventArgs const&)
+    {
+        myButton().Content(box_value(L"Clicked"));
+
+		winrt::hstring spotifyAuthUri = L"https://accounts.spotify.com/authorize";
+		winrt::hstring clientId = L"client_id=985a371b4fa24676818dc63468ca83d9";
+		winrt::hstring response_type = L"response_type=token";
+		Uri redirect_uri = WebAuthenticationBroker::GetCurrentApplicationCallbackUri();
+
+		winrt::hstring formed_uri = spotifyAuthUri + L"?" + clientId + L"&" + response_type + L"&redirect_uri=" + Uri::EscapeComponent(redirect_uri.ToString());
+
+		Uri uri(formed_uri);
+
+		auto response = co_await WebAuthenticationBroker::AuthenticateAsync(
+			WebAuthenticationOptions::None,
+			uri);
+
+		auto status = response.ResponseStatus();
+    }
+}
---
+++ spotify-breakdown-playlists-cppwinrt/MainPage.h
@@ -0,0 +1,23 @@
+﻿#pragma once
+
+#include "MainPage.g.h"
+
+namespace winrt::spotify_breakdown_playlists_cppwinrt::implementation
+{
+    struct MainPage : MainPageT<MainPage>
+    {
+        MainPage();
+
+        int32_t MyProperty();
+        void MyProperty(int32_t value);
+
+        winrt::Windows::Foundation::IAsyncAction ClickHandler(Windows::Foundation::IInspectable const& sender, Windows::UI::Xaml::RoutedEventArgs const& args);
+    };
+}
+
+namespace winrt::spotify_breakdown_playlists_cppwinrt::factory_implementation
+{
+    struct MainPage : MainPageT<MainPage, implementation::MainPage>
+    {
+    };
+}
