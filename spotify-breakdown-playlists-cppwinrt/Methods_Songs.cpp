44663249dcc15ff6765e1d6eefb6a9ff67a3742b
Debalin Das <debalin90@gmail.com>
2020-10-12 21:18:46 -0700

    Created skeleton of Songs XAML.

---
+++ spotify-breakdown-playlists-cppwinrt/Methods_Songs.cpp
@@ -0,0 +1,21 @@
+﻿#include "pch.h"
+#include "Methods_Songs.h"
+#if __has_include("Methods_Songs.g.cpp")
+#include "Methods_Songs.g.cpp"
+#endif
+
+using namespace winrt;
+using namespace Windows::UI::Xaml;
+
+namespace winrt::spotify_breakdown_playlists_cppwinrt::implementation
+{
+    Methods_Songs::Methods_Songs()
+    {
+        InitializeComponent();
+    }
+
+    Windows::Foundation::Collections::IObservableVector<IInspectable> Methods_Songs::Songs() const
+    {
+        return m_Songs;
+    }
+}
---
+++ spotify-breakdown-playlists-cppwinrt/Methods_Songs.h
@@ -0,0 +1,24 @@
+﻿#pragma once
+
+#include "Methods_Songs.g.h"
+
+namespace winrt::spotify_breakdown_playlists_cppwinrt::implementation
+{
+    struct Methods_Songs : Methods_SongsT<Methods_Songs>
+    {
+    public:
+        Methods_Songs();
+
+        Windows::Foundation::Collections::IObservableVector<IInspectable> Songs() const;
+
+    private:
+        Windows::Foundation::Collections::IObservableVector<IInspectable> m_Songs{ nullptr };
+    };
+}
+
+namespace winrt::spotify_breakdown_playlists_cppwinrt::factory_implementation
+{
+    struct Methods_Songs : Methods_SongsT<Methods_Songs, implementation::Methods_Songs>
+    {
+    };
+}
